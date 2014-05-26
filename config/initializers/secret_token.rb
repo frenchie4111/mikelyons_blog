6faac77c10939ffc4c5360d2af33a02c55930d23
Mike Lyons <mdl0394@gmail.com>
2014-05-25 22:50:27 -0400

    Added secret_token for heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MikelyonsBlog::Application.config.secret_key_base = '472644de0c230a4e86cde3cb728bed785f6aa04f3ff11c9aa69f9f203baa6e446e0bd6e467b0f2505e42fd8c875754a616d8e372e7eb95e1cd95fbcb2071e94e'
