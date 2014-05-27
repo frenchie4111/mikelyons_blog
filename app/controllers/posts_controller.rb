f67595b4326abe5c93c03a364e0afa02a1156fb5
Mike Lyons <mdl0394@gmail.com>
2014-05-26 23:21:15 -0400

    Changed ordering of posts on list screen

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.order("created_at DESC").all
     @categories = Category.all
   end
 
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
   end
 
   def show_category
-    @posts = Category.find( params[:category] ).posts.all
+    @posts = Category.find( params[:category] ).posts.order("created_at DESC").all
     @category_name = Category.find( params[:category] ).name
     @categories = Category.all
 
