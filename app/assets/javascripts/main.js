0489adc0363c1857827d80972818020470202a1c
Mike Lyons <mdl0394@gmail.com>
2014-05-28 19:43:50 -0400

    Added turbolinks fix for highlight_js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -34,3 +34,10 @@ $(document).ready(function() {
     });
     setupGallery();
 });
+
+$(document).on('page:change',  function() {
+  $('pre code').each(function(i, e) {hljs.highlightBlock(e)});
+});
+$(document).on('page:restore', function() {
+  $('pre code').each(function(i, e) {hljs.highlightBlock(e)});
+});
